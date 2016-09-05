ba48b1d7b1e5a4d2b2f8f2ea4be465f037bbd68c
Utilisateur <Utilisateur@10.13.2.128>
2016-09-06 08:45:42 +1100

    Ajout Page_d'inscription

---
+++ greenlight/page_d'inscription.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title> GREENLIGHT </title>
+	<meta name="keywords" content="projet, crowdfunding, calédonie, université" />
+	<meta name="description" content="Greenlight" />
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<!--	<link rel="stylesheet" href="style.css" /> -->
+</head>
+
+<body >
+
+<header>
+	<h1>Greenlight</h1>
+
+</header>
+
+</body>
+
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
